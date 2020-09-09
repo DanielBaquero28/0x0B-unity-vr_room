e2b7efc7965819bfcf4e51fe9af93246e9c37a70
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-09-08 19:50:59 -0500

    [Feature]: Testing v0

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public BoxCollider door;
+    Animator anim;
+    public Collider rightHand;
+    public Collider leftHand;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        door = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rightHand.bounds.Intersects(door.bounds))
+        {
+            if (anim.GetBool("Open") == false)
+                Open();
+            else
+                Close();
+        }
+
+        if (leftHand.bounds.Intersects(door.bounds))
+        {
+            if (anim.GetBool("Open") == false)
+                Open();
+            else
+                Close();
+        }
+    }
+
+    void Open()
+    {
+        anim.SetBool("Open", true);
+        anim.SetBool("Close", false);
+    }
+
+    void Close()
+    {
+        anim.SetBool("Open", false);
+        anim.SetBool("Close", true);
+    }
+}
