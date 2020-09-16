86f2d18d289c1e754dc4d7f57f623201d8ff8e43
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-09-16 15:06:20 -0500

    [Feature]: Particle system working fine

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -1,58 +1,93 @@
 ﻿using UnityEngine;
+using Valve.VR;
 
 public class DoorManager : MonoBehaviour
 {
-    public BoxCollider door;
-    Animator anim;
+    //public BoxCollider door;
+    public Animator anim;
     public GameObject rightHand;
     public GameObject leftHand;
-    void Awake()
+
+    public SteamVR_Input_Sources thisHand;
+    public SteamVR_Action_Boolean isTriggered;
+
+    public GameObject projector;
+    public GameObject particleSys;
+
+    int click = 0;
+
+
+    void Start()
     {
-        anim = gameObject.GetComponent<Animator>();
-        door = gameObject.GetComponent<BoxCollider>();
+        isTriggered.AddOnStateDownListener(TriggerDown, thisHand);
+        isTriggered.AddOnStateUpListener(TriggerUp, thisHand);
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        //Debug.Log("Trigger is Up");
+        click = 0;
+    }
+
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        //Debug.Log("Trigger is Down");
+        click = 1;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (rightHand.transform.position.z > 5.9f && rightHand.transform.position.z < 6.0f)
+        //Debug.Log(click);
+        if ((rightHand.transform.position.z > 5.8f && rightHand.transform.position.z < 6.2f && click == 1) || (leftHand.transform.position.z > 5.9f && leftHand.transform.position.z < 6.0f && click == 1))
         {
             //anim.SetBool("Open", true);
-            Debug.Log("Entered Position Condition");
+            //Debug.Log("Entered Position Condition");
             if (anim.GetBool("Open") == false)
             {
-                Debug.Log("anim.getbool('Open') == false");
+                //Debug.Log("anim.getbool('Open') == false");
                 Open();
             }
-            //else
-            //{
-                //Debug.Log("Else condition");
-                //Close();
-            //}
-
         }
 
-        // I'm not testing this condition for now
-        if (rightHand.transform.position.z > 12.0f)
+        // Testing
+        if ((rightHand.transform.position.z > 6.9f && rightHand.transform.position.z < 7.14f && click == 1) || (leftHand.transform.position.z > 6.9f && leftHand.transform.position.z < 7.14f && click == 1))
         {
-            if (anim.GetBool("Open") == false)
-                Open();
-            else
+            if (anim.GetBool("Close") == false)
+            {
+                //Debug.Log("anim.getbool('Close') == false");
                 Close();
+            }
         }
-        Debug.Log("Open Bool Val: " + anim.GetBool("Open"));
+
+
+        if (Vector3.Distance(rightHand.transform.position, projector.transform.position) <= 2.13f && Vector3.Distance(rightHand.transform.position, projector.transform.position) > 1.9f && click == 1)
+        {
+            particleSys.SetActive(true);
+        }
+        else if (Vector3.Distance(rightHand.transform.position, projector.transform.position) > 4.0f)
+        {
+            particleSys.SetActive(false);
+        }
+
+        //Debug.Log(Vector3.Distance(rightHand.transform.position, projector.transform.position));
+        //Debug.Log("Open Bool Val: " + anim.GetBool("Open"));
         //Debug.Log(rightHand.transform.position.z);
     }
 
     void Open()
     {
         anim.SetBool("Open", true);
+        anim.SetBool("Opened", true);
         anim.SetBool("Close", false);
+        anim.SetBool("Closed", false);
     }
 
     void Close()
     {
         anim.SetBool("Open", false);
+        anim.SetBool("Opened", false);
         anim.SetBool("Close", true);
+        anim.SetBool("Closed", true);
     }
 }
